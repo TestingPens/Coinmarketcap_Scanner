a8df7e11683d83ff751eb3a122c5e7171bc2104b
PariahBoy <keiran.dennie@gmail.com>
2017-12-03 10:09:54 +0200

    Fixed up Readme

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Coinmarketcap_Scanner
+Scan Coinmarketcap gainers and losers stats to detect dips/pumps. This helps for identifying trade entry and exit points and is great for those who want to BTFD.
+This application requires Python 2.7 if you're running it from source. Windows users can use the EXE version.
 
-###Installation Requirements
+### Downloading Project Source (Linux/OSX Users)
+git clone https://github.com/Prophetic-Pariah/Coinmarketcap_Scanner.git
+
+### Downloading EXE (Windows Users)
+
+
+### Installation Requirements
 pip install https://github.com/mondeja/pymarketcap/archive/master.zip
 
-###Usage
+### Usage
 1. Modify the parameters in the config.ini file
 2. Run the application: python coinmarketcap_scanner.py
--- coinmarketcap_scanner.py
+++ coinmarketcap_scanner.py
@@ -2,7 +2,7 @@
 #Requirements: pip install https://github.com/mondeja/pymarketcap/archive/master.zip
 
 from pymarketcap import Pymarketcap
-import ConfigParser
+import configparser
 import time
 
 class bcolours:
@@ -15,30 +15,31 @@ def get_stats(perc, timeframe, volume):
     losers =  coinmarketcap.ranks('losers', '1h')
     gainers = coinmarketcap.ranks('gainers', '1h')
 
-    for k1,v1 in losers.iteritems():
+    for k1,v1 in losers.items():
         for line in v1:
             if (line['percent_change'] < -perc and line['24h_volume_usd'] > volume):
-                print bcolours.RED + '[%s, %% Loss: %s, Price: $%s, Volume: $%s]' % (line['symbol'], line['percent_change'], line['price_usd'], line['24h_volume_usd']) + bcolours.ENDC
+                print(bcolours.RED + '[%s, %% Loss: %s, Price: $%s, Volume: $%s]' % (line['symbol'], line['percent_change'], line['price_usd'], line['24h_volume_usd']) + bcolours.ENDC)
     
-    for k1,v1 in gainers.iteritems():
+    for k1,v1 in gainers.items():
         for line in v1:
             if (line['percent_change'] > perc and line['24h_volume_usd'] > volume):
-                print bcolours.GREEN + '[%s, %% Gain: %s, Price: $%s, Volume: $%s]' % (line['symbol'], line['percent_change'], line['price_usd'], line['24h_volume_usd']) + bcolours.ENDC
-    print '=============================================================================='
+                print(bcolours.GREEN + '[%s, %% Gain: %s, Price: $%s, Volume: $%s]' % (line['symbol'], line['percent_change'], line['price_usd'], line['24h_volume_usd']) + bcolours.ENDC)
+    print('================================================================')
     time.sleep(timeframe)
 if __name__ == "__main__":   
     try:
-        Config = ConfigParser.ConfigParser()
+        Config = configparser.ConfigParser()
         Config.read("config.ini")
 
-        print "[*] Starting Coinmarketcap Scanner..."	
+        print("[*] Starting Coinmarketcap Scanner...")
         timeframe = int(Config.get('Config', 'Timeframe'))
         perc = int(Config.get('Config', 'Percentage'))
         volume = int(Config.get('Config', 'Volume'))
-        print "[*] Timeframe: " + str(timeframe) + "min"	
-        print "[*] Percentage: " + str(perc) + "%"
-        print "[*] Volume: $" + str(volume)
+        print("[*] Timeframe: " + str(timeframe) + "min")	
+        print("[*] Percentage: " + str(perc) + "%")
+        print("[*] Volume: $" + str(volume))
+        print('================================================================')
         while(True):
             get_stats(perc, timeframe * 60, volume)
-    except Exception, e:
-        print "[!] Error: " + str(e.message)
+    except Exception as e:
+        print("[!] Error: " + str(e.message))
